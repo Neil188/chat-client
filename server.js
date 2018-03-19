0ca9bdddd3f9559d23dab0a465aeee965d8e27c3
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-03-19 19:27:04 +0000

    Server.js changes
    Add bodyparser, http, io packages
    Set up http and socketio
    
    Use body parser json and urlencoded middleware
    
    Define get and post methods
    
    Add SIGINT processing to make sure server closes correctly on Windows

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+const http = require('http').Server(app);
+
+const io = require('socket.io')(http);
+
 
 // Pick up static file index.html
-app.use(express.static(__dirname))
-const server = app.listen(3000, () => {
+app.use(express.static(__dirname));
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'Hello'},
+];
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+})
+
+const server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
+
+// close down server on CTRL+C
+process.on("SIGINT", () => {
+    console.log("SIGINT");
+    server.close();
+});
